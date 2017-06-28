d8dcc18fb549d9f81f3418093555da78e73cacf2
akki <akki@akki.akki>
2017-06-28 22:15:59 +0530

    Completed q4

--- Assignment 3/codes/q4.py
+++ Assignment 3/codes/q4.py
@@ -3,7 +3,7 @@ import numpy as np
 import cmath as cm
 import sys
 
-input = raw_input("Enter the cofficients in DECREASING powers of x(separated by commas): ")
+input = raw_input("Enter coefficients in DECREASING powers of x(commas): ")
 input_list = input.split(',')
 numbers = [float(x.strip()) for x in input_list]
 n = len(numbers)
@@ -31,7 +31,7 @@ A = np.matrix(A)
 # print a
 
 # Printing to 3 decimal places
-np.set_printoptions(formatter={'all': lambda x: "{0:0.3f}".format(x)})
+# np.set_printoptions(formatter={'all': lambda x: "{0:0.3f}".format(x)})
 q,r =  qr(A)
 
 # print "\n\n\n"
@@ -44,7 +44,7 @@ q,r =  qr(A)
 # print np.matrix.transpose(q)*q
 # print ""
 # print q*r
-print A
+# print A
 # QR Iterations
 count = 0
 while count<2000:
@@ -59,19 +59,45 @@ eigneVals = []
 # Now extracting eigen values
 n = np.shape(A)[0]
 i = 0
-while i < n:
-	if i == n-1:
-		eigneVals.append(A[i,i])
-		i = i+1
-		continue
-	if min(abs(A[i,i+1]),abs(A[i+1,i]))>1e-5: # Non-zero Non-Diagonal
-		a = A[i,i]; b = A[i,i+1]; c = A[i+1,i]; d = A[i+1,i+1]
-		B = np.matrix([ [ a,b ],[ c,d ] ])
-		temp1 = (a+d)/2.; temp2 = cm.sqrt((a+d)**2 - 4.*(a*d-b*c))/2.
-		eigneVals.append(temp1+temp2)
-		eigneVals.append(temp1-temp2)
-		i = i+1
-	else:
-		eigneVals.append(A[i,i])
-	i = i + 1
+# while i < n:
+# 	if i == n-1:
+# 		eigneVals.append(A[i,i])
+# 		i = i+1
+# 		continue
+# 	if abs(A[i,i+1])>1e-5: # Non-zero Non-Diagonal
+# 		a = A[i,i]; b = A[i,i+1]; c = A[i+1,i]; d = A[i+1,i+1]
+# 		B = np.matrix([ [ a,b ],[ c,d ] ])
+# 		temp1 = (a+d)/2.; temp2 = cm.sqrt((a+d)**2 - 4.*(a*d-b*c))/2.
+# 		eigneVals.append(temp1+temp2)
+# 		eigneVals.append(temp1-temp2)
+# 		i = i+1
+# 	else:
+# 		eigneVals.append(A[i,i])
+# 	i = i + 1
 # print "The Solution set of the equation is:",eigneVals
+
+while i<n:
+	blockEnd = 0
+	# print "on top with i =",i
+	for j in range(i+1,n):
+		# print "on for with j=",j
+		if abs(A[j,i]) > 1e-5: #aka Non-zero
+			blockEnd = j
+	# print blockEnd
+	if blockEnd !=0:
+		B = A[i:blockEnd+1, i:blockEnd+1]
+		# print B
+		temp1, temp2 =  np.linalg.eig(B)
+		for i in range(len(temp1)):
+			eigneVals.append(temp1[i])
+		i = blockEnd+1
+		# print i
+		continue
+	# print "appending A[i,i]"
+	eigneVals.append(A[i,i])
+	i = i+1
+print "The Solution set of the equation is: ",eigneVals
+
+# for i1 in xrange(len(eigneVals)):
+#     for i2 in xrange(len(eigneVals[i1])):
+#         print eigneVals[i1][i2]
